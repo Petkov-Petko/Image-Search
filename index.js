20a73f8be1117b37c877308c0d766577202288f6
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-01 09:05:26 +0300

    did the show more button

--- index.js
+++ index.js
@@ -3,34 +3,49 @@ const searchInput = document.querySelector("#formInput");
 const photos = document.querySelector(".photos");
 const showMore = document.querySelector("#showMore");
 
-const key = "ZB_0hnsspxVAJqysauoOHuUkXe5oyo56St96zuylpHs"
+const key = "ZB_0hnsspxVAJqysauoOHuUkXe5oyo56St96zuylpHs";
 
 let keyword = "";
 let page = 1;
 
 async function searchImages() {
-    keyword = searchInput.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${key}&per_page=12`
+  keyword = searchInput.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${key}&per_page=12`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    const results = data.results;
-    results.map((el) => {
-        const image = document.createElement('img');
-        image.src = el.urls.small;
-        const imageLink = document.createElement('a');
-        imageLink.href = el.links.html;
-        imageLink.target = "_blank";
+  const results = data.results;
 
-        imageLink.appendChild(image);
-        photos.appendChild(imageLink);
-    })
-    showMore.style.display = "block";
+    if(page === 1) {
+        photos.innerHTML = "";
+    }
+
+  results.map((el) => {
+    const image = document.createElement("img");
+    image.src = el.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = el.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    photos.appendChild(imageLink);
+  });
+  showMore.style.display = "block";
 }
 
-searchForm.addEventListener('click', (e) => {
-    e.preventDefault();
-    page = 1;
-    searchImages();
-})
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
+searchForm.addEventListener("click", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMore.addEventListener("click", () => {
+  page++;
+  searchImages()
+});
